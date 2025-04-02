04f942ede3c7936d3b29af4e97088e269619b804
CaiohSantana <caiodesantana2608@outlook.com>
2025-04-01 22:15:11 -0300

    Inicio da edição do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
+import { Card, Box, Typography } from '@mui/material';
 
 
-function App() {
+export default function App() {
 
   return (
-    <div>
-      <p>
-        TESTE
-      </p>
-    </div>
+    <Box
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'flex-start'}
+      width={'100vw'}
+      height={'100vh'}
+    >
+      <Card
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          width: '100%',
+          marginTop: '144px',
+          marginX: '16px'
+        }}
+      >
+
+        {/*titulos*/}
+        <Box
+          marginY={'16px'}
+        >
+          <Typography variant='h5' fontWeight={'700'}>
+            Calculadora IMC
+          </Typography>
+        </Box>
+
+        {/*Entradas*/}
+        <Box>
+          <Typography variant='h5'>
+            Entrada
+          </Typography>
+        </Box>
+
+        {/*Resultado*/}
+        <Box>
+          <Typography variant='h5'>
+            Resultado
+          </Typography>
+        </Box>
+
+      </Card>
+    </Box>
   )
 }
-
-export default App
